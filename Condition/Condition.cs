1778f6ae74b905b8667516fe8b7a2fddec1a0ba7
Vasyl Kichula <104771020+vkich7@users.noreply.github.com>
2024-09-24 12:19:34 +0300

    Add files via upload

---
+++ Condition/Condition.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Condition
+{
+    public static class Condition
+    {
+        /// <summary>
+        /// Implement code according to description of  task 1
+        /// </summary>        
+        public static int Task1(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Implement code according to description of  task 2
+        /// </summary>  
+        public static int Task2(int n)
+        {
+            throw new NotSupportedException();
+        }
+
+    }
+}
