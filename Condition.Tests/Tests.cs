1aa570315b3c32913c315b5335063414fd91d408
Vasyl Kichula <104771020+vkich7@users.noreply.github.com>
2024-09-24 12:16:44 +0300

    Add files via upload

---
+++ Condition.Tests/Tests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Condition.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(2, 4)]
+        [TestCase(0, 0)]
+        [TestCase(-11, 11)]
+        public void Task1_ReturnCorrectValue(int n, int expected)
+        {
+            var actualResult = Condition.Task1(n);
+            if (actualResult != expected)
+            {
+                Assert.Fail(message: "Method 'Task1'  returnt incorrect value");
+            }
+        }
+
+
+        [TestCase(401, 410)]
+        [TestCase(999, 999)]
+        [TestCase(370,730)]
+        [TestCase(625,652)]
+        [TestCase(700, 700)]
+        [TestCase(109, 910)]
+        public void Task2_ReturnCorrectValue(int n, int expected)
+        {
+            var actualResult = Condition.Task2(n);
+            if (actualResult != expected)
+            {
+                Assert.Fail(message: "Method 'Task2'  returned incorrect value");
+            } 
+        }
+    }
+}
